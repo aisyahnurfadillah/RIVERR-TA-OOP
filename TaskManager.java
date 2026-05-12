f5694395651a6d37bce194acfafb8817c82deae4
Your Name <bangkitcahyalinuwih22@gmail.com>
2026-05-12 19:34:34 +0700

    penambahan add

--- TaskManager.java
+++ TaskManager.java
@@ -2,22 +2,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 //method to delete a task by id
-public class TaskManager{
+public class TaskManager {
     private List<Task> taskList = new ArrayList<>();
 
-     public void viewAllTasks(){
-          if (taskList.isEmpty()) {
-              System.out.println("No tasks available.");
-              return;
-          }
-          for (Task t : taskList) {
-              System.out.println(t.toString());
-          }
-      }
+    public void viewAllTasks() {
+        if (taskList.isEmpty()) {
+            System.out.println("No tasks available.");
+            return;
+        }
+        for (Task t : taskList) {
+            System.out.println(t.toString());
+        }
+    }
 
-    public void deleteTask(int id){
+    public void addTask(Task task) {
+        taskList.add(task);
+        System.out.println("Task added successfully!");
+    }
+
+    public void deleteTask(int id) {
         taskList.removeIf(t -> t.getId() == id);
         System.out.println("Task deleted (if existed).");
     }
 }
-
