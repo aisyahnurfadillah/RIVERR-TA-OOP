62763a62eaeea549c2f67a221008477ee8b87992
silfinanurfadilah <silfinanurfadilah@gmail.com>
2026-05-12 17:21:17 +0700

    Delete TASK-P11 directory

--- TASK-P11/TaskManager.java
+++
@@ -1,7 +0,0 @@
-  import java.util.ArrayList;
-  import java.util.List;
-
-  public class TaskManager{
-      // Data Structure to hold the objects
-      private List<Task> taskList = new ArrayList<>();
-  }
--- TASK-P11/read.java
+++
@@ -1,9 +0,0 @@
-      public void viewAllTasks(){
-          if (taskList.isEmpty()) {
-              System.out.println("No tasks available.");
-              return;
-          }
-          for (Task t : taskList) {
-              System.out.println(t.toString());
-          }
-      }
