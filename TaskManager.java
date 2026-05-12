f4192dc6d3475be33935c8af8ae97c92f020bc3e
Your Name <bangkitcahyalinuwih22@gmail.com>
2026-05-12 19:26:38 +0700

    add function read to taskmanager

--- TaskManager.java
+++ TaskManager.java
@@ -5,6 +5,16 @@ import java.util.List;
 public class TaskManager{
     private List<Task> taskList = new ArrayList<>();
 
+     public void viewAllTasks(){
+          if (taskList.isEmpty()) {
+              System.out.println("No tasks available.");
+              return;
+          }
+          for (Task t : taskList) {
+              System.out.println(t.toString());
+          }
+      }
+
     public void deleteTask(int id){
         taskList.removeIf(t -> t.getId() == id);
         System.out.println("Task deleted (if existed).");
